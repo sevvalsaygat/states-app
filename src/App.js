3c642cacaa8a539fc3eddf43995b8a4b4b035952
sevvalsaygat <sevvallsstt@gmail.com>
2022-07-15 22:15:57 +0300

    Create some components for learning to state structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("Şevval");
+  const [age, setAge] = useState(29);
+  const [friends, setFriends] = useState(["Ahmet", "Murat"]);
+  const [adress, setAddress] = useState({ title: "Istanbul", zip: 34756 });
+
+  console.log(age, name, friends);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Merhaba {name}!</h1>
+      <h2>{age}</h2>
+
+      <button onClick={() => setName("Nejdet")}>Change name</button>
+      <button onClick={() => setAge(25)}>Change age</button>
+
+      <hr />
+      <br></br>
+
+      <h2>Friends</h2>
+      {friends.map((friend, index) => (
+        <div key={index}>{friend}</div>
+      ))}
+
+      <br></br>
+
+      <button onClick={() => setFriends([...friends, "Sevde"])}>Add new friends</button>
+
+
+      <hr />
+      <br></br>
+
+      <h2>Address</h2>
+      <div>
+        {adress.title} {adress.zip}
+      </div>
+
+      <br />
+
+      <button onClick={() => setAddress({ ...adress, title: "Ankara", zip: 36100 })}>Change the address</button>
     </div>
   );
 }
